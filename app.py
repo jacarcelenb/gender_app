311b1e13e1308f3ad7e0f3a87633f5b24c45cdbe
jacarcelenb <carcelenjorge17@gmail.com>
2022-04-28 23:53:25 -0500

    creacion del modelo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import modelo
 
 app = Flask(__name__)
 
@@ -9,7 +9,24 @@ def home():
     return render_template("index.html")
 
 
+def predecir():
+    if request.method == "POST":
+        pelo = request.form['pelo']
+        anchofrente = request.form['anchofrente']
+        alturafrente = request.form['alturafrente']
+        narizancha= request.form['narizancha']
+        narizalta = request.form['narizalta']
+        labios = request.form['labios']
+        distancia = request.form['distancia']
+       
 
+        entradas = [float(pelo), float(anchofrente), float(alturafrente),
+                    float(narizancha), float(narizalta), float(labios),
+                    float(distancia)]
+
+        prediccion = modelo.predecirGenero(entradas)
+
+    return render_template("test.html", genero=prediccion)
 
 if __name__ == '__main__':
     app.run()
