88a410b0d1342f422cda5e01310eebdbb171e9c4
jacarcelenb <carcelenjorge17@gmail.com>
2022-04-29 00:30:06 -0500

    cambios en la app

--- modelo.py
+++ modelo.py
@@ -20,19 +20,19 @@ def min_max_scaler(lista):
   return listado
 
 def predecirGenero(lista):
-    # cargamos los datos
-    datos_array = np.array(lista, "float32")
+   
+    datos_array = np.array(min_max_scaler(lista), "float32")
     json_file = open('model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     # cargar pesos al nuevo modelo
     loaded_model.load_weights("model.h5")
-    opt = tf.keras.optimizers.Adam(learning_rate=0.01)
+    opt = tf.keras.optimizers.Adam(learning_rate=0.001)
     # Compilar modelo cargado y listo para usar usando los mismos parámetros.
     loaded_model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['binary_accuracy'])
 
-    return int(loaded_model.predict(datos_array.reshape(1, 2)).round())
+    return int(loaded_model.predict(datos_array.reshape(1, 7)).round())
 
 
     
