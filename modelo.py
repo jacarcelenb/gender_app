d040b5cbe98fb4f0ae0d3e07154ee6a3fc7e3a33
jacarcelenb <carcelenjorge17@gmail.com>
2022-05-05 16:21:01 -0500

    Cambios en el metodo de preprocesamiento

--- modelo.py
+++ modelo.py
@@ -1,24 +1,30 @@
 from keras.models import model_from_json
 import tensorflow as tf
 import numpy as np
+import pandas as pd
+from sklearn import decomposition
+from sklearn import preprocessing
 
 
 
-def min_max_scaler(lista):
-  listado = []
-  listamin = [0 ,11.4,5.1,0,0,0,0]
-  listamax = [1,15.5,7.1,1,1,1,1]
+def Preprocesamiento_Data(lista):
+  admissions = pd.read_csv('min_max.csv')
+  nueva_fila = pd.Series(lista, index=admissions.columns) # creamos un objeto Seris
+  admissions = admissions.append(nueva_fila, ignore_index=True)
+  datos=admissions.values
+  # aplicar el preprocesamiento de los datos
+  datos_min_max = preprocessing.MinMaxScaler().fit_transform(datos)
+  # reducir dimensiones con PCA
+  pca = decomposition.PCA(n_components=2)
+  datos_min_max = pca.fit_transform(datos_min_max)
+  data =datos_min_max[0,0:]
+  data.shape = (1,2)
+  return data
 
-  valor_min_max = 0
 
-  for i in range(len(lista)):
-    valor_min_max = (lista[i] - listamin[i])/(listamax[i] - listamin[i])
-    listado.append(valor_min_max)
-  return listado
 
 def predecirGenero(lista):
-   
-    datos_array = np.array(min_max_scaler(lista), "float32")
+    datos_array = np.array(Preprocesamiento_Data(lista), "float32")
     json_file = open('model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
@@ -28,8 +34,7 @@ def predecirGenero(lista):
     opt = tf.keras.optimizers.Adam(learning_rate=0.001)
     # Compilar modelo cargado y listo para usar usando los mismos parámetros.
     loaded_model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['binary_accuracy'])
-
-    return int(loaded_model.predict(datos_array.reshape(1, 7)).round())
+    return int(loaded_model.predict(datos_array.reshape(1, 2)).round())
 
 
     
