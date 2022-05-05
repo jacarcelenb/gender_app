82b9514edf72e0dfbe81de534ccee178e83b7508
jacarcelenb <carcelenjorge17@gmail.com>
2022-05-05 17:01:08 -0500

    Correccion de errores

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/test", methods=["POST"])
+@app.route("/deteccion", methods=["POST"])
 def predecir():
     if request.method == "POST":
         pelo = request.form['pelo']
